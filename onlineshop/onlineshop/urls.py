355c75070d771e6cc86c525b90bd79b33a7c0ef6
Tatyana Kazubovich <kazubovich.tatyana@gmail.com>
2026-09-02 17:50:34 +0300

    setup url.py in onlineshop

--- onlineshop/onlineshop/urls.py
+++ onlineshop/onlineshop/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from onlineshop import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('goods.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Администрирование магазина'
+admin.site.index_title = 'Одежда ручной вязки'
