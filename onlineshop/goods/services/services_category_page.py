95f04dbc16146dc5b06343811645b98a0beaf140
Tatyana Kazubovich <kazubovich.tatyana@gmail.com>
2026-09-18 14:22:57 +0300

    add annotation to services_category_page.py

--- onlineshop/goods/services/services_category_page.py
+++ onlineshop/goods/services/services_category_page.py
@@ -3,7 +3,7 @@ class CategoryFilterService:
     def __init__(self, category):
         self.category = category
 
-    def _get_goods(self, slug):
+    def _get_goods(self, slug:str)->list[dict]:
         check_values = 'new_model' if slug == 'new' else 'sale'
         posts = GoodsCollection.objects.filter(**{check_values: True}).order_by('-time_update')
         collect_photo = []
@@ -15,11 +15,11 @@ class CategoryFilterService:
                 collect_photo.append(ImagesGoods.objects.filter(goods__id=i.id).order_by('-time_create')[0])
         goods = [{'object': posts[i], 'photo': collect_photo[i]} for i in range(len(collect_photo))]
         return goods
-    def _get_css_class_name(self, goods):
+    def _get_css_class_name(self, goods:list[dict]) -> str:
         lenght = len(goods) if len(goods) < 4 else 4
         return 'gallery_container_' + str(lenght)
 
-    def get_collect_dict(self):
+    def get_collect_dict(self)->dict:
         context = {'goods': self._get_goods(self.category.slug),
                    'page_name': self.category.name,
                    'page_slug': self.category.slug
