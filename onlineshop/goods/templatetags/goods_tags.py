6ddfb22a2e59ffa2f436801c62fc36d41135fed3
Tatyana Kazubovich <kazubovich.tatyana@gmail.com>
2026-09-19 15:44:06 +0300

    create footer

--- onlineshop/goods/templatetags/goods_tags.py
+++ onlineshop/goods/templatetags/goods_tags.py
@@ -4,7 +4,7 @@ from goods.models import SexCategory, SeasonCategory, TypeCategory
 register = template.Library()
 
 @register.inclusion_tag('goods/main_menu.html')
-def create_main_menu():
+def create_main_menu()->dict:
 
     sex_podcat = SexCategory.objects.all()
     sex_podcat = [{'name': i.sex, 'slug': i.slug} for i in sex_podcat]
@@ -20,3 +20,20 @@ def create_main_menu():
                      {'name':'Сезон', 'slug':'season','podcats':season_podcat},
                      {'name':'Тип изделия', 'slug':'type','podcats':type_podcat},]
     return {'category_dict':category_dict}
+
+@register.simple_tag
+def get_category_dict()->list[dict]:
+    category_dict = [{'name': 'Новинки', 'slug': 'new'},
+                     {'name': 'Распродажа', 'slug': 'sale'},
+                     {'name': 'Для кого', 'slug': 'sex'},
+                     {'name': 'Сезон', 'slug': 'season'},
+                     {'name': 'Тип изделия', 'slug': 'type'}]
+    return category_dict
+
+@register.simple_tag
+def get_secondary_footer_menu()->dict:
+    secondary_menu = [{'name': 'Доставка и оплата', 'url_name':'delivery_inf'},
+                      {'name': 'Возврат и обмен', 'url_name':'exchange_inf'},
+                      {'name': 'Публичная оферта', 'url_name':'publish_oferta'},
+                      {'name': 'Контакты', 'url_name':'contacts'}]
+    return secondary_menu
